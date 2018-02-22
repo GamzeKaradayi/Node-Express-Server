1f73f0b564bd89a4f09558bfa6fef1d31cce18d7
Alper Numan Genc <alper.numan.genc@accenture.com>
2018-02-22 17:45:10 +0300

    Authorization removed while fetching houses with api get /houses

--- src/routes/house.js
+++ src/routes/house.js
@@ -43,42 +43,13 @@ const houseRouter = (db) => {
 
   // Get All Houses
   router.get('/', (req, res) => {
-    if (req.user && req.user.isAuth) {
-      console.log(`House: Request started with user ${JSON.stringify(req.user)}`);
-      if (req.user.id) {
-        db.findById(userTable, req.user.id).then((result) => {
-          const user = result && result.length ? result[0] : null;
-          if (user && user.isAdmin) {
-            db.selectAll(houseTable).then((houses) => {
-              res.status(200).json(houses);
-            }).catch((ex) => {
-              console.log('House: While fetching all houses an error occured:', ex);
-              res.status(500).send('Unexpected error occured!');
-            });
-          } else if (user && user.houseId) {
-            db.findById(houseTable, user.houseId).then((houseResult) => {
-              const foundHouse = houseResult && houseResult.length ? houseResult[0] : null;
-              res.status(200).json(foundHouse);
-            }).catch((ex) => {
-              console.log('House: While fetching house an error occured:', ex);
-              res.status(500).send('Unexpected error occured!');
-            });
-          } else if (user) {
-            res.status(200).json([]);
-          } else {
-            res.status(500).send('User not found!');
-          }
-        }).catch((ex) => {
-          console.log('House: While fetching user an error occured:', ex);
-          res.status(500).send('Unexpected error occured!');
-        });
-      } else {
-        res.status(400).send('Invalid token!');
-      }
-    } else {
-      console.log('House: Unauthorized request responded with 401.');
-      res.status(401).send('Unauthorized user!');
-    }
+    console.log(`House: Request started with user ${JSON.stringify(req.user)}`);
+    db.selectAll(houseTable).then((houses) => {
+      res.status(200).json(houses);
+    }).catch((ex) => {
+      console.log('House: While fetching all houses an error occured:', ex);
+      res.status(500).sed('Unexpected error occured');
+    });
   });
 
   // Get house with id
