94fbbafd44c8309cc5d884992a90285f8cc60c19
Alper Numan Genc <alper.numan.genc@accenture.com>
2018-03-01 19:46:43 +0300

    Regex updated with TR characters.

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,13 +1,13 @@
 // Expected name has character less than 20 and more than 3 characters
 // Should contain valid characters
 export function validFirstname(str) {
-  const regexp = RegExp('^[a-zA-Z]+([_ -]?[a-zA-Z0-9])*$');
+  const regexp = RegExp('^[a-zA-ZğüşıöçĞÜŞİÖÇ]+([_ -]?[a-zA-Z0-9ğüşıöçĞÜŞİÖÇ])*$');
   return !!str && str.length >= 3 && str.length <= 20 && regexp.test(str);
 }
 
 // Expected lastname has character less than 20 and more than 6 characters
 export function validLastname(lastname) {
-  const regexp = RegExp('^[a-zA-Z0-9_ -]*');
+  const regexp = RegExp('^[a-zA-Z0-9ğüşıöçĞÜŞİÖÇ_ -]*');
   return !!lastname && lastname.length >= 3 && lastname.length <= 20 && regexp.test(lastname);
 }
 
@@ -15,14 +15,14 @@ export function validLastname(lastname) {
 // Expected name has character less than 20 and more than 3 characters
 // Should contain valid characters
 export function validHousename(str) {
-  const regexp = RegExp('^[a-zA-Z]+([_ -]?[a-zA-Z0-9])*$');
+  const regexp = RegExp('^[a-zA-ZğüşıöçĞÜŞİÖÇ]+([_ -]?[a-zA-Z0-9ğüşıöçĞÜŞİÖÇ])*$');
   return !!str && str.length >= 3 && str.length <= 20 && regexp.test(str);
 }
 
 // Expected task description has character less than 50 and more than 3 characters
 // Should contain valid characters
 export function validTaskdescription(str) {
-  const regexp = RegExp('^[a-zA-Z]+([_ -:]?[a-zA-Z0-9 ])*$');
+  const regexp = RegExp('^[a-zA-ZğüşıöçĞÜŞİÖÇ]+([_ -:]?[a-zA-Z0-9ğüşıöçĞÜŞİÖÇ ])*$');
   console.log(regexp.test(str));
   console.log(str.length);
   return !!str && str.length >= 3 && str.length <= 50 && regexp.test(str);
