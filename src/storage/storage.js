7023efe5d65c4ecacd6f1b7c51ceb045464cd732
Alper Numan Genc <alper.numan.genc@accenture.com>
2018-03-02 11:15:45 +0300

    DB clustering removed.

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -2,104 +2,68 @@
 
 const mysql = require('mysql');
 const CONFIG = require('../config/config.js');
-let connectedClusterName;
-let connectedInstance;
+
 const connect = () => Promise.resolve().then(() => {
-  const poolCluster = mysql.createPoolCluster();
-  const clusters = Object.keys(CONFIG.DB);
-
-  clusters.forEach(cluster => {
-    poolCluster.add(cluster, {
-      host: CONFIG.DB[cluster].HOST || 'localhost',
-      user: CONFIG.DB[cluster].USER || 'root',
-      password: CONFIG.DB[cluster].PASSWORD || '123456',
-      database: CONFIG.DB[cluster].DATABASE || 'houseworkup'
-    });
+  const connection = mysql.createConnection({
+    host: CONFIG.DB.HOST || 'localhost',
+    user: CONFIG.DB.USER || 'root',
+    password: CONFIG.DB.PASSWORD || '123456',
+    database: CONFIG.DB.DATABASE || 'houseworkup'
   });
 
-  const getConnection = () => new Promise((resolve, reject) => {
-    if (connectedClusterName && connectedInstance) {
-      resolve(connectedInstance);
-    } else if (clusters.length > 0) {
-      connectedClusterName = connectedClusterName || clusters[0];
-      console.log('Trying to connect cluster ', connectedClusterName);
-      poolCluster.getConnection(connectedClusterName, (err, connection) => {
-        if (err) {
-          reject(err);
-        } else {
-          connectedInstance = connection;
-          resolve(connection);
-        }
-      });
-    } else {
-      const errMessage = 'At least have one db cluster! Check configurations.';
-      reject(errMessage);
-    }
-  });
-
-  const changeCluster = (clusterName) => {
-    if (connectedClusterName !== clusterName && clusters.indexOf(clusterName) >= 0) {
-      connectedInstance.end();
-      connectedInstance = undefined;
-      connectedClusterName = clusterName;
-    } else if (connectedClusterName === clusterName) {
-      console.log('Already connected cluster:', clusterName);
-    } else {
-      console.log('Cluster not exist: ', clusterName);
-    }
-  };
+  connection.connect();
 
   const selectAll = (table) => new Promise((resolve, reject) => {
-    getConnection().then((connection) => connection.query(`SELECT * FROM ${table.name}`, (err, rows, fields) => {
+    connection.query(`SELECT * FROM ${table.name}`, (err, rows, fields) => {
       if (err) reject(err);
       else {
         console.log('Storage: Select success table: ', table.name);
         resolve(rows);
       }
-    }));
+    });
   });
 
   const selectWithWhereCondition = (table, where) => new Promise((resolve, reject) => {
     const whereKeys = Object.keys(where).map((wk) => wk + '= ?').join(' AND ');
     const whereValues = Object.keys(where).map((wk) => where[wk]);
 
-    getConnection().then((connection) => connection.query(`SELECT * FROM ${table.name} WHERE ${whereKeys}`, whereValues, (err, rows, fields) => {
+    connection.query(`SELECT * FROM ${table.name} WHERE ${whereKeys}`, whereValues, (err, rows, fields) => {
       if (err) reject(err);
       else {
         console.log('Storage: Select with condition success table: ', table.name, ' with where ', where);
         resolve(rows);
       }
-    }));
+    });
   });
 
   const findById = (table, id) => new Promise((resolve, reject) => {
-    getConnection().then((connection) => connection.query(`SELECT * FROM ${table.name} WHERE id = ? LIMIT 1`, [id], (err, rows, fields) => {
+    connection.query(`SELECT * FROM ${table.name} WHERE id = ? LIMIT 1`, [id], (err, rows, fields) => {
       if (err) reject(err);
       else {
         console.log('Storage: Find success table: ', table.name, ' with id ', id);
         resolve(rows);
       }
-    }));
+    });
   });
 
   const deleteById = (table, id) => new Promise((resolve, reject) => {
-    getConnection().then((connection) => connection.query(`DELETE FROM ${table.name} WHERE id = ?`, [id], (err, rows, fields) => {
+    connection.query(`DELETE FROM ${table.name} WHERE id = ?`, [id], (err, rows, fields) => {
       if (err) reject(err);
       else {
         console.log('Storage: Delete success table: ', table.name, ' with id ', id);
         resolve(rows);
       }
-    }));
+    });
   });
 
   const insertRow = (table, row) => new Promise((resolve, reject) => {
-    getConnection().then((connection) => connection.query(`INSERT INTO ${table.name} SET ?`, row, (err, rows, fields) => {
+    connection.query(`INSERT INTO ${table.name} SET ?`, row, (err, rows, fields) => {
       if (err) reject(err);
       else {
         console.log('Storage: Insert success row: ', row);
         resolve(rows.insertId);
       }
-    }));
+    });
   });
 
   const updateWithWhereCondition = (table, updates, where) => new Promise((resolve, reject) => {
@@ -110,24 +74,23 @@ const connect = () => Promise.resolve().then(() => {
 
     const allValues = updateValues.concat(whereValues);
 
-    getConnection().then((connection) =>
-      connection.query(`UPDATE ${table.name} SET ${updateKeys} WHERE ${whereKeys}`, allValues, (err, rows, fields) => {
-        if (err) reject(err);
-        else {
-          console.log('Storage: Select with condition success table: ', table.name, ' with where ', where);
-          resolve();
-        }
-      }));
+    connection.query(`UPDATE ${table.name} SET ${updateKeys} WHERE ${whereKeys}`, allValues, (err, rows, fields) => {
+      if (err) reject(err);
+      else {
+        console.log('Storage: Select with condition success table: ', table.name, ' with where ', where);
+        resolve();
+      }
+    });
   });
 
   const deleteRow = (table, row) => new Promise((resolve, reject) => {
-    getConnection().then((connection) => connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
       if (err) reject(err);
       else {
         console.log('The solution is: ', rows[0].solution);
         resolve();
       }
-    }));
+    });
   });
 
   return {
@@ -137,8 +100,7 @@ const connect = () => Promise.resolve().then(() => {
     findById,
     deleteById,
     insertRow,
-    deleteRow,
-    changeCluster
+    deleteRow
   };
 }).catch((e) => {
   console.log('Storage: While connecting db instance an error occured:', e);
@@ -147,9 +109,9 @@ const connect = () => Promise.resolve().then(() => {
 
 
 // Close connection
-const quit = () => {
-  if (connectedInstance) {
-    connectedInstance.end();
+const quit = (connection) => {
+  if (connection) {
+    connection.end();
   }
 };
 
